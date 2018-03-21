8a9a5e0ba1c1a3efb464807b805228419e10af6b
Vatbon <stavaton@gmail.com>
2018-03-21 16:41:21 +0700

    Добавил main и условие задачи

---
+++ polynom/Source.cpp
@@ -0,0 +1,12 @@
+/*
+**На вход командной строки даются коэффициенты многолченов a и b
+** "a(n-1) a(n-2) ... a(1)" "b(n-1) b(n-2) ... b(1)"
+** Вывести коэффициенты 
+*/
+
+
+int main(int argc, char *argv[]) {
+	char* InputPolyA = argv[1];
+	char* InputPolyB = argv[2];
+	return 0;
+}
